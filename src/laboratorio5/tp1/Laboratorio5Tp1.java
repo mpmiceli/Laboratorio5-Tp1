b9b52d81398edc568a8211f122cfc0cb14e300eb
Maria Paula Miceli <mariapaulamiceli@gmail.com>
2018-03-19 21:56:46 -0300

    a

---
+++ src/laboratorio5/tp1/Laboratorio5Tp1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio5.tp1;
+
+/**
+ *
+ * @author Paula
+ */
+public class Laboratorio5Tp1 {
+    
+    public static void main(String[] args) 
+    {
+        BeerHouse beerHouse = new BeerHouse();
+        
+        BeerProducer antares = new BeerProducer(beerHouse, "antares");
+        antares.start();
+        BeerProducer cheverry = new BeerProducer(beerHouse, "cheverry");
+        cheverry.start();
+        
+        BeerConsumer paula = new BeerConsumer(beerHouse, "paula");
+        BeerConsumer gatin = new BeerConsumer(beerHouse, "gatin");
+        
+        
+        
+        paula.start();
+        gatin.start();
+        
+    } 
+}
